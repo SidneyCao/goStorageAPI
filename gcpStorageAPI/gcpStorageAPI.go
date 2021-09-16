1154ab6bade2b84866a503a8ca1bfe5fae85450b
Simoncao <13671707129@126.com>
2021-09-16 19:19:58 +0800

    [debug]寻找内存泄漏问题

--- gcpStorageAPI/gcpStorageAPI.go
+++ gcpStorageAPI/gcpStorageAPI.go
@@ -85,9 +85,10 @@ func main() {
 			fmt.Printf("debug %s\n", string(line))
 			waitGroup.Add(1)
 			workerChan <- string(line)
+			close(workerChan)
 			fmt.Printf("channum %d\n", len(workerChan))
 			worker(workerChan, c, &waitGroup)
-			close(workerChan)
+
 		}
 		close(workerChan)
 	}
