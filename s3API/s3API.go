203d4ccf6e8314db25f7cbf02ecc59cd17f8dc8a
Simoncao <13671707129@126.com>
2021-09-22 14:20:54 +0800

    [feat]aws s3 api flag

--- s3API/s3API.go
+++ s3API/s3API.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+//flag
+var (
+	bucket = flag.String("b", "", "bucket名 (默认为空)")
+	files  = flag.String("f", "", "文件列表 (默认为空)")
+	cache  = flag.String("c", "true", "是否缓存")
+	prefix = flag.String("p", "", "需要移除的文件前缀 (默认为空)")
+	thread = flag.Int("t", 5, "最大协程数")
+)
 
 func main() {
 	fmt.Println("starting...")
