c7f960bfead5c292f595b75cdafa823fae572c4d
root <simonsidneycao@gmail.com>
2021-12-09 13:47:58 +0800

    [feat]exclude go build file

--- getPara.sh
+++ getPara.sh
@@ -8,6 +8,35 @@ case ${game} in
                 nocacheStatus=1
                 nocacheFile=zip.min.js
         ;;
+	xiuzhen)
+                srcDir=/data/xiuzhen.17996cdn.net
+                gstore=gs://xiuzhen-cdn
+                gStatus=1
+                nocacheStatus=0
+                nocacheFile=none
+        ;;
+	ysr)
+                srcDir=/data/ysr.17996cdn.net
+                gstore=gs://ysr-cdn
+                gStatus=1
+                nocacheStatus=0
+                nocacheFile=none
+        ;;
+	yisu)
+                srcDir=/data/yisu.17996cdn.net
+                gstore=gs://yisu-cdn
+                gStatus=1
+                nocacheStatus=1
+                nocacheFile='^.*server.cfg$|announcement.cfg$|updateinfo.xml$'
+        ;;
+        baoxiang)
+                srcDir=/data/baoxiang.17996cdn.net
+                gstore=gs://baoxiang-17996cdn-net
+                gStatus=1
+                nocacheStatus=0
+                nocacheFile=none
+#                nocacheFile=".*test.txt|abc$"
+        ;;	
         *)
                 echo "${game}  is not exist"
         ;;
